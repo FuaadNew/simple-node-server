0cfb61208eb7a7b50815737cb1e0d47f353fb8cf
Fuaad <fuaadmohamoud@gmail.com>
2025-01-10 10:35:41 -0600

    added mongodb schema to index.js

--- index.js
+++ index.js
@@ -5,6 +5,24 @@ const cors = require('cors')
 app.use(cors())
 app.use(express.static('dist'))
 
+
+const mongoose = require('mongoose')
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
 let notes = [
     {
       id: "1",
--- mongo.js
+++ mongo.js
@@ -4,7 +4,7 @@ if (process.argv.length<3){
     process.exit(1)
 }
 const password = process.argv[2]
-const url = `mongodb+srv://fuaadmohamoud:${hJDZUtGNTbNpthu8}@cluster0.u3oaq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const url = `mongodb+srv://fuaadmohamoud:${password}@cluster0.u3oaq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 mongoose.set('strictQuery',false)
 mongoose.connect(url)
 
@@ -16,12 +16,19 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model('Note',noteSchema)
 
-const note = newNote({
+const note = new Note({
     content: 'HTML is easy',
     important: true
 })
 
-note.save().then(result => {
-    console.log('note saved!')
+Note.find({important:true}).then(result => {
+    result.forEach(note=>{
+        console.log(note)
+    })
     mongoose.connection.close()
 })
+
+//note.save().then(result => {
+  //  console.log('note saved!')
+  //  mongoose.connection.close()
+// })
