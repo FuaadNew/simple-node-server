629ba823d02ae85ff1f928632f731a2da208a548
Fuaad <fuaadmohamoud@gmail.com>
2025-01-15 08:11:02 -0600

    reverted changes

--- index.js
+++ index.js
@@ -4,34 +4,87 @@ app.use(express.json())
 const cors = require('cors')
 app.use(cors())
 app.use(express.static('dist'))
-require('dotenv').config();
 
 
+const mongoose = require('mongoose')
 
 const password = process.argv[2]
 
 
+const url = `mongodb+srv://fuaadmohamoud:${password}@cluster0.u3oaq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
 
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
 
-const Note = require('./models/note')
+const Note = mongoose.model('Note', noteSchema)
 
+<<<<<<< HEAD
 
+=======
+let notes = [
+    {
+      id: "1",
+      content: "HTML is easy",
+      important: true
+    },
+    {
+      id: "2",
+      content: "Browser can execute only JavaScript",
+      important: false
+    },
+    {
+      id: "3",
+      content: "GET and POST are the most important methods of HTTP protocol",
+      important: true
+    }
+  ]
+>>>>>>> parent of 73e5315 (fixed route bug)
 
 app.get('/',(request,response)=>{
   response.send('<h1>Hello World</h1>')
 })
 
 app.get('/api/notes',(request,response)=>{
-  Note.find({}).then(notes=>{
-    response.json(notes)
-
-  })
+  response.json(notes)
 })
 
+app.delete('/api/notes/:id',(request,response) => {
+  const id = request.params.id
+  notes = notes.find(note=>note.id != id)
+  response.status('204').end()
 
 
+<<<<<<< HEAD
 app.post('/api/notes',(request,response,next)=>{
+=======
+})
+
+app.get('/api/notes/:id',(request,response)=>{
+  const id = request.params.id
+  const note = notes.find(note=>note.id == id)
+  if (note){
+    response.json(note)
+  }else{
+    response.status(400).end()
+  }
+  
+
+})
+
+const generateId = () =>{
+  const maxId = notes.length >0
+  ? Math.max(...notes.map(n=>number(n.id))) 
+  : 0
+  return String(maxId + 1)
+}
+
+app.post('api/notes',(request,response)=>{
+>>>>>>> parent of 73e5315 (fixed route bug)
   
   const body = request.body
   if (!body.content){
@@ -39,16 +92,24 @@ app.post('/api/notes',(request,response,next)=>{
       error:'content missing'
     })
   }
-  const note = new Note({
+  const note = {
     content: body.content,
+<<<<<<< HEAD
     important: body.important || false
   })
 
   note.save().then(savedNote =>{
     response.json(savedNote)
   }).catch(error => next(error))
+=======
+    important: Boolean(body.important) || false,
+    id: generateId()
+  }
+>>>>>>> parent of 73e5315 (fixed route bug)
  
+  notes = notes.concat(note)
   
+<<<<<<< HEAD
 })
 app.get('/api/notes/:id',(request,response)=>{
   Note.findById(request.params.id)
@@ -75,10 +136,14 @@ app.put('/api/notes/:id', (request, response, next) => {
     response.json(updatedNote)
   })
   .catch(error =>(error))
+=======
+  response.json(note)
+>>>>>>> parent of 73e5315 (fixed route bug)
 
 })
 
 
+<<<<<<< HEAD
 app.delete('/api/notes/:id',(request,response) => {
   const id = request.params.id
   notes = notes.filter(note=>note.id != id)
@@ -99,10 +164,11 @@ const errorHandler = (error,request,response,next)=>{
 }
 next(error)
 
+=======
+>>>>>>> parent of 73e5315 (fixed route bug)
 
 app.use(errorHandler)
 const PORT = process.env.PORT ||  3000
 app.listen(PORT, ()=>{
   console.log(`server running at ${PORT}`)
 })
-
